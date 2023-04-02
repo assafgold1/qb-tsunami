3bb9a483ec65ef647b16225f770e8422607cb060
Corazon <41511738+assafgold1@users.noreply.github.com>
2023-04-02 07:56:18 +0300

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+description 'QBCore Tsunami'
+name 'qb-tsunami'
+version '1.0.0'
+
+client_scripts {
+    'client/main.lua'
+}
+
+server_scripts {
+    'server/main.lua'
+}
+
+dependencies {
+    'qb-core',
+    'qb-phone'
+}
+
